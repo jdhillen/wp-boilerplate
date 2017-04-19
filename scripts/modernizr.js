572dcc1872cb2e84d4798ba6b18c24fa981501d5
J.D. Hillen <jhillen01@gmail.com>
2017-04-19 13:25:04 -0500

    Update - Removed Modernizr.sh for Modernizr.js with shelljs

---
+++ scripts/modernizr.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+'use strict';
+
+
+/* ==|== Required =============================================================================== */
+var shell = require('shelljs');
+
+
+/* ==|== Generate a new Modernizr.js build ====================================================== */
+shell.echo('Generating a new build of Modernizr.js with all options');
+shell.exec('./node_modules/modernizr/bin/modernizr -c ./node_modules/modernizr/lib/config-all.json -d ./node_modules/modernizr -u');
--- scripts/modernizr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# modernizr.sh
-
-# Generate a new Modernizr.js build
-echo "Generating a new build of Modernizr.js with all options";
-
-./node_modules/modernizr/bin/modernizr -c ./node_modules/modernizr/lib/config-all.json -d ./node_modules/modernizr -u

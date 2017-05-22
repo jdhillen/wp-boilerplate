3e8b6d7874c4b9ef882e3afbc8af47b10d7d8fc5
J.D. Hillen <jhillen@dimin.com>
2017-05-22 15:45:29 -0500

    Working - Style Guide

--- src/page-templates/style-guide.php
+++ src/page-templates/style-guide.php
@@ -141,8 +141,8 @@
 
 
     <section id="text-inline">
-      <h2 class="docs-heading" data-magellan-target="text-inline"><a href="#text-inline"></a>Inline Elements</h2>
-      <div>
+        <h2>Inline Elements</h2>
+
         <p><a href="#!">This is a text link</a>.</p>
 
         <p><strong>Strong is used to indicate strong importance.</strong></p>
@@ -178,240 +178,245 @@
         <p>The <var>variable element</var>, such as <var>x</var> = <var>y</var>.</p>
 
         <p>The time element: <time datetime="2013-04-06T12:32+00:00">2 weeks ago</time></p>
-      </div>
     </section>
 
 
     <section id="embedded-images">
-      <h2 class="docs-heading" data-magellan-target="embedded-images"><a href="#embedded-images"></a>Images</h2>
-      <div>
-        <h6>No <code>&lt;figure&gt;</code> element</h6>
-        <img src="http://placehold.it/350x350" alt="Image alt text">
-      </div>
-      <div>
-        <h6>Wrapped in a <code>&lt;figure&gt;</code> element, no <code>&lt;figcaption&gt;</code></h6>
-        <figure>
-          <img src="http://placehold.it/350x350" alt="Image alt text">
-        </figure>
-      </div>
-      <div>
-        <h6>Wrapped in a <code>&lt;figure&gt;</code> element, with a <code>&lt;figcaption&gt;</code></h6>
-        <figure>
-          <img src="http://placehold.it/350x350" alt="Image alt text">
-          <figcaption>Here is a caption for this image.</figcaption>
-        </figure>
-      </div>
+        <h2>Images</h2>
+
+        <div>
+            <p>No <code>&lt;figure&gt;</code> element</p>
+            <img src="http://placehold.it/350x350" alt="Image alt text">
+        </div>
+
+        <div>
+            <p>Wrapped in a <code>&lt;figure&gt;</code> element, no <code>&lt;figcaption&gt;</code></p>
+            <figure>
+                <img src="http://placehold.it/350x350" alt="Image alt text">
+            </figure>
+        </div>
+        <div>
+            <p>Wrapped in a <code>&lt;figure&gt;</code> element, with a <code>&lt;figcaption&gt;</code></p>
+            <figure>
+                <img src="http://placehold.it/350x350" alt="Image alt text">
+                <figcaption>Here is a caption for this image.</figcaption>
+            </figure>
+        </div>
     </section>
 
 
     <section id="embedded-audio">
-      <h2 class="docs-heading" data-magellan-target="embedded-audio"><a href="#embedded-audio"></a>Audio</h2>
-      <div>
-        <audio controls="">
-          <source src="https://p4.bcbits.com/download/track/a369abd6636fa2831cabab8a18d54ee6/mp3-128/2592979997?fsig=d16d43050aac5035344c86df360f3909&id=2592979997&stream=1&ts=1494374400.0&token=1494374460_508567aa0e1dec53b99f74403095dd449da20e9c" type="audio/mp3">
+        <h2>Audio</h2>
+        <audio controls>
+            <source src="https://www.w3schools.com/html/horse.mp3" type="audio/mpeg">
+            <source src="https://www.w3schools.com/html/horse.ogg" type="audio/ogg">
+            Your browser does not support the audio tag.
         </audio>
-      </div>
     </section>
 
 
     <section id="embedded-video">
-      <h2 class="docs-heading" data-magellan-target="embedded-video"><a href="#embedded-video"></a>Video</h2>
-      <div>
+        <h2>Video</h2>
         <video controls="">
-          <source src="http://html5demos.com/assets/dizzy.mp4" type="video/mp4">
-          <source src="http://html5demos.com/assets/dizzy.webm" type="video/webm">
-          <source src="http://html5demos.com/assets/dizzy.ogv" type="video/ogg">
+            <source src="http://html5demos.com/assets/dizzy.mp4" type="video/mp4">
+            <source src="http://html5demos.com/assets/dizzy.webm" type="video/webm">
+            <source src="http://html5demos.com/assets/dizzy.ogv" type="video/ogg">
+            Your browser does not support the video tag.
         </video>
-      </div>
     </section>
 
 
     <section id="embedded-canvas">
-      <h2 class="docs-heading" data-magellan-target="embedded-canvas"><a href="#embedded-canvas"></a>Canvas</h2>
-      <div>
-        <canvas id="myCanvas" width="300" height="250" style="background-color: #333333;">
-          <p>Your browser does not support canvas.</p>
-        </canvas>
-      </div>
+        <h2>Canvas</h2>
+        <canvas id="myCanvas">Your browser does not support canvas.</canvas>
+        <script>
+            var c = document.getElementById("myCanvas");
+            var ctx = c.getContext("2d");
+            ctx.fillStyle = "#FF0000";
+            ctx.fillRect(0, 0, 300, 250);
+        </script>
     </section>
 
 
     <section id="embedded-meter">
-      <h2 class="docs-heading" data-magellan-target="embedded-meter"><a href="#embedded-meter"></a>Meter</h2>
-      <div>
+        <h2>Meter</h2>
         <meter value="2" min="0" max="10">2 out of 10</meter>
-      </div>
     </section>
 
 
     <section id="embedded-progress">
-      <h2 class="docs-heading" data-magellan-target="embedded-progress"><a href="#embedded-progress"></a>Progress</h2>
-      <div>
+        <h2>Progress</h2>
         <progress value="22" max="100"></progress>
-      </div>
     </section>
 
 
     <section id="embedded-svg">
-      <h2 class="docs-heading" data-magellan-target="embedded-svg"><a href="#embedded-svg"></a>SVG</h2>
-      <div>
-        <svg width="100px" height="100px">
-          <circle cx="100" cy="100" r="100" fill="#1fa3ec"></circle>
+        <h2>SVG</h2>
+        <svg width="200px" height="200px">
+            <circle cx="100" cy="100" r="100" fill="#1fa3ec"></circle>
         </svg>
-      </div>
     </section>
 
 
     <section id="embedded-iframe">
-      <h2 class="docs-heading" data-magellan-target="embedded-iframe"><a href="#embedded-iframe"></a>iFrame</h2>
-      <div>
-        <iframe src="http://www.w3schools.com">
-          <p>Your browser does not support iframes.</p>
-        </iframe>
-      </div>
+        <h2>iFrame</h2>
+        <iframe src="http://www.w3schools.com">Your browser does not support iframes.</iframe>
     </section>
 
 
     <section id="form-input">
-      <h2 class="docs-heading" data-magellan-target="form-input"><a href="#form-input"></a>Input Fields</h2>
-      <form>
-        <fieldset>
-          <legend>Input Field Legend</legend>
+        <h2>Form &amp; Input Fields</h2>
+        <form>
+            <fieldset>
+                <legend>Input Field Legend</legend>
 
-          <label for="input-text">Text Input</label>
-          <input id="input-text" type="text" placeholder="Text Input">
+                <label for="input-text">Text Input</label>
+                <input id="input-text" type="text" placeholder="Text Input">
 
-          <label for="input-password">Password</label>
-          <input id="input-password" type="password" placeholder="Type your Password">
+                <label for="input-password">Password</label>
+                <input id="input-password" type="password" placeholder="Type your Password">
 
-          <label for="input-webaddress">Web Address</label>
-          <input id="input-webaddress" type="url" placeholder="http://yoursite.com">
+                <label for="input-webaddress">Web Address</label>
+                <input id="input-webaddress" type="url" placeholder="http://yoursite.com">
 
-          <label for="input-emailaddress">Email Address</label>
-          <input id="input-emailaddress" type="email" placeholder="name@email.com">
+                <label for="input-emailaddress">Email Address</label>
+                <input id="input-emailaddress" type="email" placeholder="name@email.com">
 
-          <label for="input-phone">Phone Number</label>
-          <input id="input-phone" type="tel" placeholder="(999) 999-9999">
+                <label for="input-phone">Phone Number</label>
+                <input id="input-phone" type="tel" placeholder="(999) 999-9999">
 
-          <label for="input-search">Search</label>
-          <input id="input-search" type="search" placeholder="Enter Search Term">
+                <label for="input-search">Search</label>
+                <input id="input-search" type="search" placeholder="Enter Search Term">
 
-          <label for="input-text2">Number Input</label>
-          <input id="input-text2" type="number" placeholder="Enter a Number">
+                <label for="input-text2">Number Input</label>
+                <input id="input-text2" type="number" placeholder="Enter a Number">
 
-          <label for="input-text3" class="error">Error</label>
-          <input id="input-text3" class="is-error" type="text" placeholder="Text Input">
+                <label for="input-text3" class="error">Error</label>
+                <input id="input-text3" class="is-error" type="text" placeholder="Text Input">
 
-          <label for="input-text4" class="valid">Valid</label>
-          <input id="input-text4" class="is-valid" type="text" placeholder="Text Input">
-        </fieldset>
-      </form>
+                <label for="input-text4" class="valid">Valid</label>
+                <input id="input-text4" class="is-valid" type="text" placeholder="Text Input">
+            </fieldset>
+        </form>
     </section>
 
 
     <section id="form-select">
-      <h2 class="docs-heading" data-magellan-target="form-select"><a href="#form-select"></a>Select Menus</h2>
-      <form>
-        <fieldset>
-          <legend>Select Menu Legend</legend>
+        <h2>Select Menus</h2>
+        <form>
+            <fieldset>
+                <legend>Select Menu Legend</legend>
 
-          <label for="select">Select</label>
-          <select id="select">
-            <option>Option One</option>
-            <option>Option Two</option>
-            <option>Option Three</option>
-          </select>
-        </fieldset>
-      </form>
+                <label for="select">Select</label>
+                <select id="select">
+                    <option>Option One</option>
+                    <option>Option Two</option>
+                    <option>Option Three</option>
+                </select>
+            </fieldset>
+        </form>
     </section>
 
 
     <section id="form-checkbox">
-      <h2 class="docs-heading" data-magellan-target="form-checkbox"><a href="#form-checkbox"></a>Checkboxes</h2>
-      <form>
-        <fieldset>
-          <legend>Checkbox Legend</legend>
+        <h2>Checkboxes</h2>
+        <form>
+            <fieldset>
+                <legend>Checkbox Legend</legend>
 
-          <label for="checkbox1"><input id="checkbox1" name="checkbox" type="checkbox" checked="checked"> Choice A</label>
-          <label for="checkbox2"><input id="checkbox2" name="checkbox" type="checkbox"> Choice B</label>
-          <label for="checkbox3"><input id="checkbox3" name="checkbox" type="checkbox"> Choice C</label>
-        </fieldset>
-      </form>
+                <label for="checkbox1">
+                    <input id="checkbox1" name="checkbox" type="checkbox" checked="checked"> Choice A
+                </label>
+                <label for="checkbox2">
+                    <input id="checkbox2" name="checkbox" type="checkbox"> Choice B
+                </label>
+                <label for="checkbox3">
+                    <input id="checkbox3" name="checkbox" type="checkbox"> Choice C
+                </label>
+            </fieldset>
+        </form>
     </section>
 
 
     <section id="form-radio">
-      <h2 class="docs-heading" data-magellan-target="form-radio"><a href="#form-radio"></a>Radio Buttons</h2>
-      <form>
-        <fieldset>
-          <legend>Radio Button Legend</legend>
+        <h2>Radio Buttons</h2>
+        <form>
+            <fieldset>
+                <legend>Radio Button Legend</legend>
 
-          <label for="radio1"><input id="radio1" name="radio" type="radio" class="radio" checked="checked"> Option 1</label>
-          <label for="radio2"><input id="radio2" name="radio" type="radio" class="radio"> Option 2</label>
-          <label for="radio3"><input id="radio3" name="radio" type="radio" class="radio"> Option 3</label>
-        </fieldset>
-      </form>
+                <label for="radio1">
+                    <input id="radio1" name="radio" type="radio" class="radio" checked="checked"> Option 1
+                </label>
+                <label for="radio2">
+                    <input id="radio2" name="radio" type="radio" class="radio"> Option 2
+                </label>
+                <label for="radio3">
+                    <input id="radio3" name="radio" type="radio" class="radio"> Option 3
+                </label>
+            </fieldset>
+        </form>
     </section>
 
 
     <section id="form-textareas">
-      <h2 class="docs-heading" data-magellan-target="form-textareas"><a href="#form-textareas"></a>Textareas</h2>
-      <form>
-        <fieldset>
-          <legend>Textarea Legend</legend>
+        <h2>Textareas</h2>
+        <form>
+            <fieldset>
+                <legend>Textarea Legend</legend>
 
-          <label for="textarea">This is the Textarea Label</label>
-          <textarea id="textarea" rows="8" cols="48" placeholder="Enter your message here"></textarea>
-        </fieldset>
-      </form>
+                <label for="textarea">This is the Textarea Label</label>
+                <textarea id="textarea" rows="8" cols="48" placeholder="Enter your message here"></textarea>
+            </fieldset>
+        </form>
     </section>
 
 
     <section id="form-html5inputs">
-      <h2 class="docs-heading" data-magellan-target="form-html5inputs"><a href="#form-html5inputs"></a>HTML5 Inputs</h2>
-      <form>
-        <fieldset>
-          <legend>HTML5 Input Legend</legend>
+        <h2>HTML5 Inputs</h2>
+        <form>
+            <fieldset>
+                <legend>HTML5 Input Legend</legend>
 
-          <label for="ic">Color input</label>
-          <input type="color" id="ic" value="#000000">
+                <label for="ic">Color input</label>
+                <input type="color" id="ic" value="#000000">
 
-          <label for="in">Number input</label>
-          <input type="number" id="in" min="0" max="10" value="5">
+                <label for="in">Number input</label>
+                <input type="number" id="in" min="0" max="10" value="5">
 
-          <label for="ir">Range input</label>
-          <input type="range" id="ir" value="10">
+                <label for="ir">Range input</label>
+                <input type="range" id="ir" value="10">
 
-          <label for="idd">Date input</label>
-          <input type="date" id="idd" value="1970-01-01">
+                <label for="idd">Date input</label>
+                <input type="date" id="idd" value="1970-01-01">
 
-          <label for="idm">Month input</label>
-          <input type="month" id="idm" value="1970-01">
+                <label for="idm">Month input</label>
+                <input type="month" id="idm" value="1970-01">
 
-          <label for="idw">Week input</label>
-          <input type="week" id="idw" value="1970-W01">
+                <label for="idw">Week input</label>
+                <input type="week" id="idw" value="1970-W01">
 
-          <label for="idt">Datetime input</label>
-          <input type="datetime" id="idt" value="1970-01-01T00:00:00Z">
+                <label for="idt">Datetime input</label>
+                <input type="datetime" id="idt" value="1970-01-01T00:00:00Z">
 
-          <label for="idtl">Datetime-local input</label>
-          <input type="datetime-local" id="idtl" value="1970-01-01T00:00">
-        </fieldset>
-      </form>
+                <label for="idtl">Datetime-local input</label>
+                <input type="datetime-local" id="idtl" value="1970-01-01T00:00">
+            </fieldset>
+        </form>
     </section>
 
 
     <section id="form-action">
-      <h2 class="docs-heading" data-magellan-target="form-action"><a href="#form-action"></a>Action Buttons</h2>
-      <form>
-        <fieldset>
-          <legend>Action Button Legend</legend>
-          <input class="button" type="submit" value="Input">
-          <button class="button" type="submit">Button</button>
-          <input class="button" type="reset" value="Reset">
-          <input class="button disabled" type="submit" value="Disabled" >
-        </fieldset>
-      </form>
+        <h2>Action Buttons</h2>
+        <form>
+            <fieldset>
+                <legend>Action Button Legend</legend>
+
+                <input class="button" type="submit" value="Input">
+                <button class="button" type="submit">Button</button>
+                <input class="button" type="reset" value="Reset">
+                <input class="button disabled" type="submit" value="Disabled" >
+            </fieldset>
+        </form>
     </section>
 
 </main><!-- Close page -->
